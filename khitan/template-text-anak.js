6fdcc9a9b4ea8b5116bc94b7fb0f138889bbc003
dwimartins <dwimartinsinambela19@gmail.com>
2026-03-29 14:01:21 +0700

    Add files via upload

---
+++ khitan/template-text-anak.js
@@ -0,0 +1,105 @@
+// File ini khusus untuk menyimpan template pengantar WA dan Tutorial untuk acara ANAK (Khitan, Aqiqah, Ulang Tahun).
+// Anda bebas mengedit teks di dalam tanda kutip miring ( ` ) sesuka hati seperti di notepad.
+
+window.dataTemplateWAAnak = {
+
+    khitan_islam: `Yth. Bapak/Ibu/Saudara/i __ Di Tempat 
+
+السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللّٰهِ وَبَرَكَاتُهُ
+
+Dengan memohon rahmat dan ridho Allah Subhanahu Wa Ta'ala, kami mengundang Bapak/Ibu/Saudara/i untuk hadir dalam acara Tasyakuran Khitan anak kami:
+
+===============
+👦 *[nama_anak]* 👦
+===============
+
+Untuk informasi detail acara, Bapak/Ibu/Saudara/i dapat mengakses undangan digital kami pada link berikut: 👇👇👇
+https://web.undangandari.com/[link slug]
+
+_(Note: Disarankan menggunakan browser Chrome)_
+
+Merupakan suatu kebahagiaan bagi kami bila Bapak/Ibu/Saudara/i berkenan meringankan langkah untuk hadir maupun memberikan doa restu agar anak kami menjadi anak yang sholeh.
+Atas perhatian dan doanya, kami mengucapkan banyak terima kasih.
+
+وَالسَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللّٰهِ وَبَرَكَاتُهُ
+
+🙏 Hormat Kami,
+Keluarga Bpk. [nama_orang_tua]`,
+
+
+    aqiqah_islam: `Yth. Bapak/Ibu/Saudara/i __ Di Tempat 
+
+السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللّٰهِ وَبَرَكَاتُهُ
+
+Alhamdulillah, telah lahir buah hati kami tercinta. Dengan memohon rahmat dan ridho Allah Subhanahu Wa Ta'ala, kami mengundang Bapak/Ibu/Saudara/i untuk hadir dalam acara Tasyakuran Aqiqah anak kami:
+
+===============
+👶 *[nama_anak]* 👶
+===============
+
+Untuk informasi detail acara, Bapak/Ibu/Saudara/i dapat mengakses undangan digital kami pada link berikut: 👇👇👇
+https://web.undangandari.com/[link slug]
+
+_(Note: Disarankan menggunakan browser Chrome)_
+
+Merupakan suatu kebahagiaan bagi kami bila Bapak/Ibu/Saudara/i berkenan meringankan langkah untuk hadir maupun memberikan doa restu.
+Atas perhatian dan doanya, kami mengucapkan banyak terima kasih.
+
+وَالسَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللّٰهِ وَبَرَكَاتُهُ
+
+🙏 Hormat Kami,
+Keluarga Bpk. [nama_orang_tua]`,
+
+
+    umum: `Yth. Bapak/Ibu/Saudara/i ________ Di Tempat 
+
+Dengan penuh sukacita, kami mengundang Bapak/Ibu/Saudara/i beserta keluarga untuk hadir dalam acara Tasyakuran / Perayaan anak kami:
+
+===============
+🎉 *[nama_anak]* 🎉
+===============
+
+Untuk informasi detail acara, Bapak/Ibu/Saudara/i dapat mengakses undangan digital kami pada link berikut: 👇👇👇
+https://web.undangandari.com/[link slug]
+
+_(Note: Disarankan menggunakan browser Chrome)_
+
+Merupakan suatu kebahagiaan bagi kami bila Bapak/Ibu/Saudara/i berkenan hadir dan memberikan doa restu untuk anak kami tercinta. 
+Atas perhatian dan kehadirannya, kami mengucapkan banyak terima kasih.
+
+🙏 Hormat Kami,
+Keluarga Bpk. [nama_orang_tua]`,
+
+
+    tutorial: `Cara membuat custom nama tamu: 🪶📝
+
+[1] Kakak klik link yang admin bagikan dibawah ini
+
+https://undangandari.com/custom-nama-tamu/?link=https://web.undangandari.com/[link slug]
+
+[2] Kakak tuliskan semua nama tamu di kolom Nama Tamu
+[3] Kakak tulis nama Anak di kolom Nama Mempelai/Anak
+[4] Kakak pilih opsi tema pengantar WA untuk mengundang pakai kata pengantar khusus (Khitan/Aqiqah/Umum)
+[5] Kakak klik tombol "Generate Nama Tamu"
+[6] Kakak klik tombol share "Whatsapp" atau bisa juga salin text
+
+_note: untuk penulisan nama tamu tidak wajib, tapi opsional ya kak_`,
+
+
+    // === Template Quotes & Ayat Suci Khusus Anak ===
+    quotes: {
+        khitan: { 
+            isi: '"Sempurnakanlah putra kami dalam ibadahnya setelah melakukan Khitanan ini. Jadikan dia anak yang selalu dalam lindungan-Mu ya Allah, jadi anak yang shalih, hafal Al-Qur\'an, faham dalam agama dan diberkahi kehidupannya didunia dan di akhirat, aamiin."', 
+            author: "- Doa Tasyakuran Khitan -" 
+        },
+        aqiqah: { 
+            isi: '"Setiap anak tergadaikan dengan aqiqahnya, disembelihkan (kambing) untuknya pada hari ketujuh, dicukur rambutnya dan diberi nama."', 
+            author: "- HR. Abu Dawud, An Nasai, Ibnu Majah -" 
+        },
+        umum: { 
+            isi: '"Anak adalah anugerah terindah dan titipan berharga dari Sang Pencipta. Semoga kelak menjadi anak yang berbakti, membawa kebahagiaan, dan menjadi cahaya bagi keluarga."', 
+            author: "- Doa & Harapan -" 
+        }
+    }
+
+};
