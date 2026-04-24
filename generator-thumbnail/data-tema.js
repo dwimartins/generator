7f122583633740e3b0471b086202d45b5e6ec412
dwimartins <dwimartinsinambela19@gmail.com>
2026-04-24 16:01:24 +0700

    Add files via upload

---
+++ generator-thumbnail/data-tema.js
@@ -0,0 +1,90 @@
+// File: data-tema.js
+
+const daftarTema = {
+    // 1. TEMA BABY BLUE (Default kita saat ini)
+    "baby-blue": {
+        nama_tema: "Baby Blue Elegan",
+        template_url: "https://ik.imagekit.io/asetundangan/aset/template-thumbnail/tanpa-foto-baby-blue-thumbnail.jpeg",
+        teks: {
+            judul: {
+                y: 200, size: 45, font: "Cinzel", weight: "400",
+                color1: "#64748b", color2: "#475569"
+            },
+            nama: {
+                y: 310, size: 150, font: "Dancing Script", weight: "700",
+                color1: "#92bddd", color2: "#6d97b3"
+            },
+            lokasi: {
+                y: 595, size: 45, font: "Italiana", weight: "600",
+                color1: "#5b8fa9", color2: "#386175"
+            }
+        }
+    },
+
+    // 2. TEMA Gold Shades
+    "gold-shades": {
+        nama_tema: "Gold Shades",
+        // Nanti ganti dengan URL gambar template maroon Anda:
+        template_url: "https://ik.imagekit.io/asetundangan/aset/template-thumbnail/tanpa-foto-gold-shades-thumbnail.jpeg", 
+        teks: {
+            judul: {
+                y: 200, size: 45, font: "Cinzel", weight: "400",
+                color1: "#b68a43", color2: "#896629"
+            },
+            nama: {
+                y: 310, size: 150, font: "Dancing Script", weight: "700",
+                color1: "#d9b36d", color2: "#ba843d" // Warna emas
+            },
+            lokasi: {
+                y: 595, size: 45, font: "Italiana", weight: "600",
+                color1: "#91652b", color2: "#a67036"
+            }
+        }
+    },
+    
+        // 3. TEMA Pink Shades
+    "pink-shades": {
+        nama_tema: "Pink Shades",
+        // Nanti ganti dengan URL gambar template maroon Anda:
+        template_url: "https://ik.imagekit.io/asetundangan/aset/template-thumbnail/tanpa-foto-pink-shades-thumbnail.jpg", 
+        teks: {
+            judul: {
+                y: 200, size: 45, font: "Cinzel", weight: "400",
+                color1: "#e08ca8", color2: "#c6718a"
+            },
+            nama: {
+                y: 310, size: 150, font: "Dancing Script", weight: "700",
+                color1: "#e39296", color2: "#bb5772" // Warna emas
+            },
+            lokasi: {
+                y: 595, size: 45, font: "Italiana", weight: "600",
+                color1: "#c16d7f", color2: "#d35079"
+            }
+        }
+    },
+    
+    // 4. TEMA Green Shades
+    "green-shades": {
+        nama_tema: "Green Shades",
+        // Nanti ganti dengan URL gambar template maroon Anda:
+        template_url: "https://ik.imagekit.io/asetundangan/aset/template-thumbnail/tanpa-foto-green-shades-thumbnail.jpg", 
+        teks: {
+            judul: {
+                y: 200, size: 45, font: "Cinzel", weight: "400",
+                color1: "#2f9a52", color2: "#379c1c"
+            },
+            nama: {
+                y: 310, size: 150, font: "Dancing Script", weight: "700",
+                color1: "#3bb05e", color2: "#1d8b25" // Warna emas
+            },
+            lokasi: {
+                y: 595, size: 45, font: "Italiana", weight: "600",
+                color1: "#58964a", color2: "#0f6709"
+            }
+        }
+    }
+
+
+    
+    // Anda bisa menambahkan tema ketiga, keempat, dst di bawah sini...
+};
